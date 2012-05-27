0e30c312810a1855e9f0a230bdc9a166f437d115
Marco Walther <mw46d@kenai.com>
2012-05-26 19:37:44 -0700

    The RAILS_ROOT was missing

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,5 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
